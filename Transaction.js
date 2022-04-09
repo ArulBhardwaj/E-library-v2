2c24e67d86cba2fea931bb6f3d15fed3539a0533
ArulBhardwaj <88354537+ArulBhardwaj@users.noreply.github.com>
2022-04-08 17:50:56 -0700

    Add files via upload

---
+++ Transaction.js
@@ -0,0 +1,83 @@
+import React, {Component} from "react";
+import {View,Text,StyleSheet,TouchableOpacity} from "react-native";
+import * as Permissions from "expo-permissions";
+import {BarCodeScanner} from "expo-barcode-scanner";
+
+export default class TransactionScreen extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      domState: "normal",
+      hasCameraPermissions:null,
+      scanned:false,
+      scannedData:""
+    };
+  }
+  getCameraPermissions=async domState=>{
+    const{status}=await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      /*status === "granted" is true when uses has granted permission
+      status === "granted" is false when user has not granted the permission
+      */
+      hasCameraPermissions:status==="granted",
+      domState:domState,
+      scanned:false
+    });
+  };
+  handleBarCodeScanned=async({type,data})=> {
+    this.setState({
+      scammedData:data,
+      domState:"normal",
+      scanned:true
+    });
+  };
+  render(){
+    const{domState,hasCameraPermissions,scannedData,scanned}=this.state;
+    if(domState==="scanned"){
+      return(
+        <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined:this.handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+    return(
+      <View style={styles.container}>
+      <Text style={styles.text}>
+      {hasCameraPermissions ? scannedData:"Request for Camera Permission"}
+      </Text>
+      <TouchableOpacity
+      style={[styles.button,{marginTop:25}]}
+      onPress={()=> this.getCameraPermission("scanner")}
+      >
+      <Text style={styles.buttonText}>Scan QR Code</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 15
+  },
+  button: {
+    width: "50%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF"
+  }
+});
